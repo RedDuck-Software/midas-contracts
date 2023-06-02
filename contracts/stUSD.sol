6a0123d5d4133c6eb8d57b9e9d119aa9c7d636d1
kostyamospan <kostya04042001@gmail.com>
2023-06-02 12:44:20 +0300

    chore: st usd mint/burn

--- contracts/stUSD.sol
+++ contracts/stUSD.sol
@@ -7,12 +7,27 @@ import "./access/Blacklistable.sol";
 
 contract stUSD is ERC20Pausable, Blacklistable {
     bytes32 public constant TERMS_URL_METADATA_KEY = keccak256("urls.terms");
+
     bytes32 public constant DESCRIPTION_URL_METADATA_KEY =
         keccak256("urls.description");
 
     mapping(bytes32 => bytes) public metadata;
 
     constructor(address _ac) ERC20("stUSD", "stUSD") Blacklistable(_ac) {}
+
+    function mint(
+        address to,
+        uint256 amount
+    ) external onlyRole(ST_USD_MINT_OPERATOR_ROLE, msg.sender) {
+        _mint(to, amount);
+    }
+
+    function burn(
+        address from,
+        uint256 amount
+    ) external onlyRole(ST_USD_BURN_OPERATOR_ROLE, msg.sender) {
+        _burn(from, amount);
+    }
 
     function pause() external onlyRole(ST_USD_PAUSE_OPERATOR_ROLE, msg.sender) {
         _pause();
