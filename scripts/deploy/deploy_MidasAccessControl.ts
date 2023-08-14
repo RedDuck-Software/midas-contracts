32e09268071894e1af193f5c961dd8d9d4f3f43f
m000nler <oleksii.rychko@redduck.io>
2023-08-14 11:20:28 +0300

    fix: format

--- scripts/deploy/deploy_MidasAccessControl.ts
+++ scripts/deploy/deploy_MidasAccessControl.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from "hardhat";
+import { ethers, upgrades } from 'hardhat';
 import * as hre from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
