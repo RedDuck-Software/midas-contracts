72af2c4503b6a93549902cd45b096894269cf43b
kostyamospan <kostya04042001@gmail.com>
2024-01-17 14:16:33 +0200

    Revert "fix: healthy diff in data feed changed to 3 days"
    This reverts commit 89cb8ef8999ae2a6323a220255594b3393203ae8.

--- contracts/feeds/DataFeed.sol
+++ contracts/feeds/DataFeed.sol
@@ -24,7 +24,7 @@ contract DataFeed is WithMidasAccessControl, IDataFeed {
     /**
      * @dev healty difference between `block.timestamp` and `updatedAt` timestamps
      */
-    uint256 private constant _HEALTHY_DIFF = 3 days;
+    uint256 private constant _HEALTHY_DIFF = 1 days;
 
     /**
      * @inheritdoc IDataFeed
