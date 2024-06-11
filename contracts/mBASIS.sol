d05212ce54f6952398423698842fb74b7b720a7f
kostyamospan <kostya04042001@gmail.com>
2024-06-11 09:48:34 +0300

    fix: mTBILL/mBASIS refactoring to follow DRY

--- contracts/mBASIS.sol
+++ contracts/mBASIS.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
-
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
-
-import "./interfaces/IMTbill.sol";
-import "./access/Blacklistable.sol";
+import "./mTBILL.sol";
 
 /**
  * @title mBASIS
  * @author RedDuck Software
  */
 //solhint-disable contract-name-camelcase
-contract mBASIS is ERC20PausableUpgradeable, Blacklistable, IMTbill {
+contract mBASIS is mTBILL {
     /**
      * @notice actor that can mint mBASIS
      */
@@ -31,11 +27,6 @@ contract mBASIS is ERC20PausableUpgradeable, Blacklistable, IMTbill {
         keccak256("M_BASIS_PAUSE_OPERATOR_ROLE");
 
     /**
-     * @notice metadata key => metadata value
-     */
-    mapping(bytes32 => bytes) public metadata;
-
-    /**
      * @dev leaving a storage gap for futures updates
      */
     uint256[50] private __gap;
@@ -44,78 +35,29 @@ contract mBASIS is ERC20PausableUpgradeable, Blacklistable, IMTbill {
      * @notice upgradeable pattern contract`s initializer
      * @param _accessControl address of MidasAccessControll contract
      */
-    function initialize(address _accessControl) external initializer {
+    function initialize(address _accessControl) external override initializer {
         __Blacklistable_init(_accessControl);
         __ERC20_init("Midas Basis Trading Token", "mBASIS");
     }
 
     /**
-     * @inheritdoc IMTbill
+     * @dev AC role, owner of which can mint mBASIS token
      */
-    function mint(
-        address to,
-        uint256 amount
-    ) external onlyRole(M_BASIS_MINT_OPERATOR_ROLE, msg.sender) {
-        _mint(to, amount);
+    function _minterRole() internal pure override returns (bytes32) {
+        return M_BASIS_MINT_OPERATOR_ROLE;
     }
 
     /**
-     * @inheritdoc IMTbill
+     * @dev AC role, owner of which can burn mBASIS token
      */
-    function burn(
-        address from,
-        uint256 amount
-    ) external onlyRole(M_BASIS_BURN_OPERATOR_ROLE, msg.sender) {
-        _burn(from, amount);
+    function _burnerRole() internal pure override returns (bytes32) {
+        return M_BASIS_BURN_OPERATOR_ROLE;
     }
 
     /**
-     * @inheritdoc IMTbill
+     * @dev AC role, owner of which can pause mBASIS token
      */
-    function pause()
-        external
-        override
-        onlyRole(M_BASIS_PAUSE_OPERATOR_ROLE, msg.sender)
-    {
-        _pause();
-    }
-
-    /**
-     * @inheritdoc IMTbill
-     */
-    function unpause()
-        external
-        override
-        onlyRole(M_TBILL_PAUSE_OPERATOR_ROLE, msg.sender)
-    {
-        _unpause();
-    }
-
-    /**
-     * @inheritdoc IMTbill
-     */
-    function setMetadata(
-        bytes32 key,
-        bytes memory data
-    ) external onlyRole(DEFAULT_ADMIN_ROLE, msg.sender) {
-        metadata[key] = data;
-    }
-
-    /**
-     * @dev overrides _beforeTokenTransfer function to ban
-     * blaclisted users from using the token functions
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    )
-        internal
-        virtual
-        override(ERC20PausableUpgradeable)
-        onlyNotBlacklisted(from)
-        onlyNotBlacklisted(to)
-    {
-        ERC20PausableUpgradeable._beforeTokenTransfer(from, to, amount);
+    function _pauserRole() internal pure override returns (bytes32) {
+        return M_BASIS_PAUSE_OPERATOR_ROLE;
     }
 }
--- contracts/mTBILL.sol
+++ contracts/mTBILL.sol
@@ -26,7 +26,7 @@ contract mTBILL is ERC20PausableUpgradeable, Blacklistable, IMTbill {
      * @notice upgradeable pattern contract`s initializer
      * @param _accessControl address of MidasAccessControll contract
      */
-    function initialize(address _accessControl) external initializer {
+    function initialize(address _accessControl) external virtual initializer {
         __Blacklistable_init(_accessControl);
         __ERC20_init("mTBILL", "mTBILL");
     }
@@ -36,7 +36,7 @@ contract mTBILL is ERC20PausableUpgradeable, Blacklistable, IMTbill {
      */
     function mint(address to, uint256 amount)
         external
-        onlyRole(M_TBILL_MINT_OPERATOR_ROLE, msg.sender)
+        onlyRole(_minterRole(), msg.sender)
     {
         _mint(to, amount);
     }
@@ -46,7 +46,7 @@ contract mTBILL is ERC20PausableUpgradeable, Blacklistable, IMTbill {
      */
     function burn(address from, uint256 amount)
         external
-        onlyRole(M_TBILL_BURN_OPERATOR_ROLE, msg.sender)
+        onlyRole(_burnerRole(), msg.sender)
     {
         _burn(from, amount);
     }
@@ -54,22 +54,14 @@ contract mTBILL is ERC20PausableUpgradeable, Blacklistable, IMTbill {
     /**
      * @inheritdoc IMTbill
      */
-    function pause()
-        external
-        override
-        onlyRole(M_TBILL_PAUSE_OPERATOR_ROLE, msg.sender)
-    {
+    function pause() external override onlyRole(_pauserRole(), msg.sender) {
         _pause();
     }
 
     /**
      * @inheritdoc IMTbill
      */
-    function unpause()
-        external
-        override
-        onlyRole(M_TBILL_PAUSE_OPERATOR_ROLE, msg.sender)
-    {
+    function unpause() external override onlyRole(_pauserRole(), msg.sender) {
         _unpause();
     }
 
@@ -98,5 +90,26 @@ contract mTBILL is ERC20PausableUpgradeable, Blacklistable, IMTbill {
         }
 
         ERC20PausableUpgradeable._beforeTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * @dev AC role, owner of which can mint mTBILL token
+     */
+    function _minterRole() internal pure virtual returns (bytes32) {
+        return M_TBILL_MINT_OPERATOR_ROLE;
+    }
+
+    /**
+     * @dev AC role, owner of which can burn mTBILL token
+     */
+    function _burnerRole() internal pure virtual returns (bytes32) {
+        return M_TBILL_BURN_OPERATOR_ROLE;
+    }
+
+    /**
+     * @dev AC role, owner of which can pause mTBILL token
+     */
+    function _pauserRole() internal pure virtual returns (bytes32) {
+        return M_TBILL_PAUSE_OPERATOR_ROLE;
     }
 }
