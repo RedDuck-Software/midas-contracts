3e54f13581ec1b4ea4c83a32a7d16482d0678075
kostyamospan <kostya04042001@gmail.com>
2024-09-24 11:37:23 +0300

    chore: mTBILL dv config

--- scripts/deploy/mTBILL/deploy_DV.ts
+++ scripts/deploy/mTBILL/deploy_DV.ts
@@ -21,15 +21,15 @@ const configs: Record<number, DeployDvConfig> = {
     minAmount: parseUnits('0.01'),
     variationTolerance: parseUnits('0.1', 2),
   },
-  // 1: {
-  //   feeReceiver: '0x',
-  //   tokensReceiver: '0x',
-  //   instantDailyLimit: parseUnits('TODO'),
-  //   instantFee: parseUnits('TODO', 2),
-  //   minMTokenAmountForFirstDeposit: parseUnits('TODO'),
-  //   minAmount: parseUnits('TODO'),
-  //   variationTolerance: parseUnits('TODO', 2),
-  // },
+  1: {
+    feeReceiver: '0x875c06A295C41c27840b9C9dfDA7f3d819d8bC6A',
+    tokensReceiver: '0x1Bd4d8D25Ec7EBA10e94BE71Fd9c6BF672e31E06',
+    instantDailyLimit: parseUnits('1000'),
+    instantFee: parseUnits('0.1', 2),
+    minMTokenAmountForFirstDeposit: parseUnits('0'),
+    minAmount: parseUnits('0.1'),
+    variationTolerance: parseUnits('0.1', 2),
+  },
 };
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
